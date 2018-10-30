9f152f9009b63b029ecde9e66c66553a2d5e86d1
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-10-30 00:19:33 -0300

    Implement privateQueueConcurrentDownload with Concurrent async Queue

--- ConcurrencyExample/ViewController.swift
+++ ConcurrencyExample/ViewController.swift
@@ -94,6 +94,59 @@ class ViewController: UIViewController {
     }
     
     @IBAction func privateQueueConcurrentDownload(_ sender: UIButton) {
+        // Creamos la cola Concurrente de trabajo
+//        let myConcurretQueue = DispatchQueue(label: "io.keepcoding.concurrent", attributes: .concurrent)
+        // Salvo necesitemos identificar la cola con un label por alguna razón en particular, deberiamos utilizar las global Queue
+        let myConcurrentQueue = DispatchQueue.global(qos: .userInitiated)
+        
+        
+        // asignamos tareas asincronas a la cola
+        
+        myConcurrentQueue.async {
+            let data1 = try! Data(contentsOf: URL(string: self.imageURL01)!)
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.image01.image = UIImage(data: data1)
+            }
+        }
+        
+        myConcurrentQueue.async {
+            let data2 = try! Data(contentsOf: URL(string: self.imageURL02)!)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let `self` = self else { return }
+                
+                self.image02.image = UIImage(data: data2)
+            }
+        }
+        
+        myConcurrentQueue.async {
+            let data3 = try! Data(contentsOf: URL(string: self.imageURL03)!)
+            DispatchQueue.main.async {
+                self.image03.image = UIImage(data: data3)
+            }
+        }
+        
+        myConcurrentQueue.async {
+            let data4 = try! Data(contentsOf: URL(string: self.imageURL04)!)
+            DispatchQueue.main.async {
+                self.image04.image = UIImage(data: data4)
+            }
+            
+        }
+        myConcurrentQueue.async {
+            let data5 = try! Data(contentsOf: URL(string: self.imageURL05)!)
+            DispatchQueue.main.async {
+                self.image05.image = UIImage(data: data5)
+            }
+        }
+        
+        myConcurrentQueue.async {
+            let data6 = try! Data(contentsOf: URL(string: self.imageURL06)!)
+            DispatchQueue.main.async {
+                self.image06.image = UIImage(data: data6)
+            }
+        }
     }
     
     @IBAction func asyncDataDownload(_ sender: UIButton) {
